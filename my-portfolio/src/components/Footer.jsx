1e1d73eb3c117487202dc689dad3cc2d933fbe47
Chris R <136107779+CWOChris@users.noreply.github.com>
2024-03-04 15:05:32 -0600

    got the footer and header components to function

--- my-portfolio/src/components/Footer.jsx
+++ my-portfolio/src/components/Footer.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
 
 const Footer = () => {
     return (
         <Box component="footer" sx={{ bgcolor: 'background.paper', p: 6 }} textAlign="center">
             <Typography variant='subtitle1' align='center' color='text.secondary' component='p'>
-                &copy {new Date().getFullYear()} My Portfolio - <a href="https://github.com/CWOChris" target='_blank' rel='noopener noreferrer' style={{color: 'inherit'}}>@CWOChris</a>.
+                &copy; {new Date().getFullYear()} My Portfolio - <Link href="https://github.com/CWOChris" target='_blank' rel='noopener noreferrer' style={{color: 'inherit'}}>@CWOChris</Link>.
             </Typography>
         </Box>
     );
 };
+
+export default Footer;

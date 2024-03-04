897c706cd4c824be283794cca9978c5b27c044ad
Chris R <136107779+CWOChris@users.noreply.github.com>
2024-03-04 16:01:36 -0600

    changed routing in App.js to include basename=/operum-ostensae in order to accomodate the GH Pages deployment

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -10,7 +10,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/operum-ostensae">
       <Header />
       <Routes>
         <Route path='/' element={<HomePage />} />

9d898971031c4666b7f13532cb5c50aebc66b45a
Chris R <136107779+CWOChris@users.noreply.github.com>
2024-03-04 16:15:27 -0600

    change to HomePage.jsx to accomodate linking in GH Pages

--- my-portfolio/src/components/HomePage.jsx
+++ my-portfolio/src/components/HomePage.jsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
+import { Link } from'react-router-dom';
 
 function HomePage() {
   return (
     <div>
       <h1>Welcome to my portfolio</h1>
       <p>Feel free to browse around and learn more about me.</p>
-      <Button variant="contained" color="primary" href="/about">
+      <Button variant="contained" color="primary" component={Link} to="/about">
         About Me
       </Button>
     </div>

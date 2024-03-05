a8d7f327e140cab4323477b493709a728eaa6d26
Chris R <136107779+CWOChris@users.noreply.github.com>
2024-03-05 00:17:55 -0600

    Placed the image of me next to the text in my Resume.jsx component

--- my-portfolio/src/components/AboutPage.jsx
+++ my-portfolio/src/components/AboutPage.jsx
@@ -6,16 +6,19 @@ import Container from "@mui/material/Container";
 const AboutPage = () => {
     return (
         <Container component="main" maxWidth="md">
-            <Box sx={{ marginTop: 8, display: "flex", flexDirection: "column", alignItems: "center" }}>
-                <Typography component="h1" variant="h4" gutterBottom>
-                    About Me
-                </Typography>
-                <Typography variant="body1" paragraph>
-                    I am a full-stack web developer with a passion for building and maintaining web applications. I have experience with HTML, CSS, JavaScript, React, Node.js, Express, and MongoDB. I am also familiar with SQL. I am a lifelong learner and am always looking for opportunities to expand my skill set.
-                </Typography>
-                <Typography variant="body1" paragraph>
-                    I am a United States Army veteran with over 10 years of service. I have experience in leadership, management, and training. I am a team player and am comfortable working in a team environment or independently. I am a problem solver and am comfortable working in high-stress and fast-paced environments.
-                </Typography>
+            <Box sx={{ marginTop: 8, display: "flex", flexDirection: "row", alignItems: "flex-start" }}>
+                <img src="/assets/CWOChris.jpg" alt="Description" style={{ height: '300px', marginRight: '20px' }} />
+                <Box>
+                    <Typography component="h1" variant="h4" gutterBottom>
+                        About Me
+                    </Typography>
+                    <Typography variant="body1" paragraph>
+                        I am a full-stack web developer with a passion for building and maintaining web applications. I have experience with HTML, CSS, JavaScript, React, Node.js, Express, and MongoDB. I am also familiar with SQL. I am a lifelong learner and am always looking for opportunities to expand my skill set.
+                    </Typography>
+                    <Typography variant="body1" paragraph>
+                        I am a United States Army veteran with over 10 years of service. I have experience in leadership, management, and training. I am a team player and am comfortable working in a team environment or independently. I am a problem solver and am comfortable working in high-stress and fast-paced environments.
+                    </Typography>
+                </Box>
             </Box>
         </Container>
     );

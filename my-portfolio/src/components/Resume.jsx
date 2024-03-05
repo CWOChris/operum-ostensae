7ba8a2fa878ce1b47984768b3997b55908a90520
Chris R <136107779+CWOChris@users.noreply.github.com>
2024-03-05 00:11:07 -0600

    added top padding to move the entire content of Resume.jsx down to accomodate for making the position of the Header.jsx component fixed

--- my-portfolio/src/components/Resume.jsx
+++ my-portfolio/src/components/Resume.jsx
@@ -9,50 +9,52 @@ import Typography from "@mui/material/Typography";
 
 const Resume = () => {
     return (
-        <Container component="main" maxWidth="md">
-            <Box sx={{ my: 4 }}>
-                <Typography variant="h4" component="h1" gutterBottom>
-                    My Resume
-                </Typography>
-
-                <Paper elevation={3} sx={{ my: 4, p: 2 }}>
-                    <Typography variant="h6" component="h2">
-                        Work Experience
+        <Box sx={{ paddingTop: '64px' }}>
+            <Container component="main" maxWidth="md">
+                <Box sx={{ my: 4 }}>
+                    <Typography variant="h4" component="h1" gutterBottom>
+                        My Resume
                     </Typography>
-                    <List>
-                        <ListItem>
-                            <ListItemText primary="Blackhawk Pilot" secondary="US Army - January 2010 to December 2016" />
-                        </ListItem>
-                        {/* Add more job experience(s) here as the example above indicates to */}
-                    </List>
-                </Paper>
 
-                <Paper elevation={3} sx={{ my: 4, p: 2 }}>
-                    <Typography variant="h6" component="h2">
-                        Education
-                    </Typography>
-                    <List>
-                        <ListItem>
-                            <ListItemText primary="Full Stack Web Development" secondary="U of MN Full Stack Web Development Coding BootCamp by EdX - 2024" />
-                        </ListItem>
-                        {/* Add more education experience(s) here as the example above indicates to */}
-                    </List>
-                </Paper>
+                    <Paper elevation={3} sx={{ my: 4, p: 2 }}>
+                        <Typography variant="h6" component="h2">
+                            Work Experience
+                        </Typography>
+                        <List>
+                            <ListItem>
+                                <ListItemText primary="Blackhawk Pilot" secondary="US Army - January 2010 to December 2016" />
+                            </ListItem>
+                            {/* Add more job experience(s) here as the example above indicates to */}
+                        </List>
+                    </Paper>
 
-                <Paper elevation={3} sx={{ my: 4, p: 2 }}>
-                    <Typography variant="h6" component="h2">
-                        Skills
-                    </Typography>
-                    <List>
-                        <ListItem>
-                            <ListItemText primary="HTML, CSS, JavaScript, React, Node.js, Express, MongoDB, SQL" />
-                        </ListItem>
-                        {/* Add more skills here as the example above indicates to */}
-                    </List>
-                </Paper>
-                {/* Add more sections as applicable with time */}
-            </Box>
-        </Container>
+                    <Paper elevation={3} sx={{ my: 4, p: 2 }}>
+                        <Typography variant="h6" component="h2">
+                            Education
+                        </Typography>
+                        <List>
+                            <ListItem>
+                                <ListItemText primary="Full Stack Web Development" secondary="U of MN Full Stack Web Development Coding BootCamp by EdX - 2024" />
+                            </ListItem>
+                            {/* Add more education experience(s) here as the example above indicates to */}
+                        </List>
+                    </Paper>
+
+                    <Paper elevation={3} sx={{ my: 4, p: 2 }}>
+                        <Typography variant="h6" component="h2">
+                            Skills
+                        </Typography>
+                        <List>
+                            <ListItem>
+                                <ListItemText primary="HTML, CSS, JavaScript, React, Node.js, Express, MongoDB, SQL" />
+                            </ListItem>
+                            {/* Add more skills here as the example above indicates to */}
+                        </List>
+                    </Paper>
+                    {/* Add more sections as applicable with time */}
+                </Box>
+            </Container>
+        </Box>
     );
 };
 

6d6d0fe1c8c55b03284621e1debcbba8b5156b65
Chris R <136107779+CWOChris@users.noreply.github.com>
2024-03-10 18:59:35 -0500

    fixed links to work correctly in Footer.jsx

--- my-portfolio/src/components/Footer.jsx
+++ my-portfolio/src/components/Footer.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import { Box, Typography, Link as MuiLink } from '@mui/material';
-import { Link } from'react-router-dom';
+import { Box, Typography } from '@mui/material';
+import { Link as RouterLink } from 'react-router-dom'; // Import React Router's Link as RouterLink
 
 const Footer = () => {
     return (
         <Box component="footer" sx={{ bgcolor: 'background.paper', p: 6 }} textAlign="center">
             <Typography variant='subtitle1' align='center' color='text.secondary' component='p'>
-                &copy; {new Date().getFullYear()} My Portfolio - <Link href="https://github.com/CWOChris" target='_blank' rel='noopener noreferrer' style={{color: 'inherit'}}>@CWOChris</Link>.
+                &copy; {new Date().getFullYear()} My Portfolio - 
+                <a href="https://github.com/CWOChris" target='_blank' rel='noopener noreferrer' style={{color: 'inherit', textDecoration: 'none'}}>
+                    CWOChris
+                </a>.
             </Typography>
             <Typography variant='subtitle1' align='center' color='text.secondary' component='p'>
-                <Link to='/credits' component={MuiLink} style={{color: 'inherit'}}>Credits</Link>
+                <RouterLink to="/credits" style={{color: 'inherit', textDecoration: 'none'}}>
+                    Credits
+                </RouterLink>
             </Typography>
         </Box>
     );
